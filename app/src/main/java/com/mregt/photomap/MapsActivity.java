8c39ccec1a35457f297f6c3bb71ad130127ae22b
mregtej <mregtej@gmail.com>
2017-08-07 00:45:34 +0200

    Added demo navigation menu and first javadoc commit

--- app/src/main/java/com/mregt/photomap/MapsActivity.java
+++ app/src/main/java/com/mregt/photomap/MapsActivity.java
@@ -1,9 +1,18 @@
 package com.mregt.photomap;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,12 +21,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+/**
+ *
+ */
+/**
+ * <p>PhotoMap main screen.<br>
+ * Displays Google Maps + App navigation menu.</p>
+ * <p>Main functionalities:</p>
+ * <p>Bugs: Not yet tested.</p>
+ *
+ * @author Modesto Rego
+ */
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
+        NavigationView.OnNavigationItemSelectedListener{
 
     /** Class name (Logging) */
     private static final String TAG = MapsActivity.class.getName();
 
-    /** GoogleMap instance*/
+    /** GoogleMap instance  */
     private GoogleMap mMap;
 
     @Override
@@ -28,24 +49,119 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        if(mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        } else {
-            Log.e(TAG,"Map fragment not found!");
-        }
+                .findFragmentById(R.id.fr_map);
+
+        // Obtain ActionBar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /* FloatingActionButtons*/
+        /* - Location button    */
+        /* - Camera button      */
+        // Set listener for location button. TODO Action shall be defined
+        FloatingActionButton locationButton = (FloatingActionButton) findViewById(R.id.fb_location);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // Set listener for camera button. TODO Action shall be defined
+        FloatingActionButton cameraButton = (FloatingActionButton) findViewById(R.id.fb_camera);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Ontain DrawerLayout, inflate NavigationView and set NavigationItemSelectedListener
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Set MapListener (Async events)
+        mapFragment.getMapAsync(this);
+
         Log.d(TAG,"onCreate() - finished.");
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
+     * <p>Manipulates the map once available.<br>
+     * This callback is triggered when the map is ready to be used.</p>
+     *
+     * <p>Current functionalities:<br>
+     *     + Adds a marker near Sydney, Australia.
+     * </p>
+     *
+     * <p>If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * installed Google Play services and returned to the app.</p>
+     *
+     * @param   googleMap   Google Map instance.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
